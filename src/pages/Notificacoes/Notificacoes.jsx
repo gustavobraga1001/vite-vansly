31e3ebea34d55453b8f89d5c2be6404813393baa
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-05-02 09:36:06 -0300

    update

--- src/pages/Notificacoes/Notificacoes.jsx
+++ src/pages/Notificacoes/Notificacoes.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
 import returnImg from "../../assets/Return.svg";
-import telaNotify from "../../assets/tela-notify.svg"
-import HeaderFixo from '../../components/HeaderFixo/headerFixo';
-import "./Notificacoes.css"
+import telaNotify from "../../assets/tela-notify.svg";
+import HeaderFixo from "../../components/HeaderFixo/headerFixo";
+import "./Notificacoes.css";
 
 const Notificacoes = () => {
   return (
-    <div className='notificacao-container'>
-        <HeaderFixo tela={"home"} img={returnImg} text={"Notificações"} />
-        <div className='notificacao-box'>
-            <img src={telaNotify} alt="" />
-            <p>Você ainda não recebeu nenhuma notificação</p>
-        </div>
+    <div className="notificacao-container">
+      <HeaderFixo tela={"home"} img={returnImg} text={"Notificações"} />
+      <div className="notificacao-box">
+        <img src={telaNotify} alt="" />
+        <p>Você ainda não recebeu nenhuma notificação</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notificacoes
+export default Notificacoes;
