130597f7b0e3a86e0d10661c6738499dc4485444
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-09-02 15:27:05 -0300

    implements page absence finally

--- src/pages/Faltas/Faltas.jsx
+++ src/pages/Faltas/Faltas.jsx
@@ -1,21 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import Footer from "../../components/Footer";
 import HeaderFixo from "../../components/HeaderFixo/headerFixo";
+import { UserCircleMinus, DotsThreeVertical } from "@phosphor-icons/react";
 
 import imgFalta from "../../assets/img-falta.svg";
 
 import "./Faltas.css";
-import { Link } from "react-router-dom";
 
 export function Faltas() {
+  const [ausencias, setAusencias] = useState([]);
+
+  useEffect(() => {
+    const prevAusencias = JSON.parse(localStorage.getItem("ausencias"));
+
+    if (prevAusencias) {
+      setAusencias(prevAusencias);
+    }
+  }, []); // O array de dependências está vazio para rodar o efeito apenas uma vez
+
   return (
     <div>
       <HeaderFixo text={"Informar ausência"} tela="home" />
       <main className="main-faltas">
-        <img src={imgFalta} alt="" />
-        <h2>Nenhuma ausência registrada</h2>
-        <span>
-          Até o momento você não informou nenhuma ausência a seu motorista.
-        </span>
+        {ausencias.length > 0 ? (
+          <>
+            <p className="title-faltas">Ausencias</p>
+            <div className="list-faltas">
+              {ausencias.map((data) => {
+                return (
+                  <div className="card-faltas" key={data}>
+                    <div>
+                      <UserCircleMinus size={36} color="#003B6D" />
+                      <span>{data}</span>
+                    </div>
+                    <DotsThreeVertical
+                      size={32}
+                      color="#AAAAAA"
+                      weight="bold"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <img src={imgFalta} alt="Nenhuma ausência registrada" />
+            <h2>Nenhuma ausência registrada</h2>
+            <span>
+              Até o momento você não informou nenhuma ausência a seu motorista.
+            </span>
+          </>
+        )}
 
         <button>
           <Link to={"informar-faltas"}>Informar ausência</Link>
--- src/pages/Faltas/InformarFalta/InformarFalta.jsx
+++ src/pages/Faltas/InformarFalta/InformarFalta.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
-import Footer from "../../../components/Footer";
 import HeaderFixo from "../../../components/HeaderFixo/headerFixo";
 
 import "./InformarFalta.css";
 
 export function InfomarFalta() {
   const [ativado, setAtivado] = useState(false);
+  const [dataAusencia, setDataAusencia] = useState("");
 
   const handleClick = () => {
     setAtivado(!ativado);
   };
 
+  function formatDate(dateString) {
+    const [year, month, day] = dateString.split("-");
+    return `${day}/${month}/${year}`;
+  }
+
+  function handleNewFalta(e) {
+    e.preventDefault();
+    if (dataAusencia === "") {
+      return;
+    }
+
+    const prevAusencias = JSON.parse(localStorage.getItem("ausencias"));
+
+    if (prevAusencias) {
+      localStorage.setItem(
+        "ausencias",
+        JSON.stringify([...prevAusencias, dataAusencia])
+      );
+    } else {
+      localStorage.setItem("ausencias", JSON.stringify([dataAusencia]));
+    }
+
+    setDataAusencia("")
+  }
+
   return (
     <div>
       <HeaderFixo text={"Informar ausência"} tela="faltas" />
       <main className="main-informar-falta">
-        <div className="box-ausensia">
+        {/* <div className="box-ausensia">
           <span>Ausência recorrente</span>
           <div
             className={`toggle-container ${ativado ? "ativo" : ""}`}
@@ -23,27 +48,17 @@ export function InfomarFalta() {
           >
             <div className="toggle-ball"></div>
           </div>
-        </div>
+        </div> */}
 
-        {!ativado ? (
-          <form>
-            <label htmlFor="">Data da ausência</label>
-            <input type="date" name="" id="" />
-          </form>
-        ) : (
-          ""
-        )}
-
-        <div>
-          <div>
-            <span>Ausencias</span>
-            <div>
-              icone
-              <span>19/09/2024</span>
-              img
-            </div>
-          </div>
-        </div>
+        <form onSubmit={handleNewFalta}>
+          <label>Data da ausência</label>
+          <input
+            type="date"
+            onChange={(e) => setDataAusencia(e.target.value)}
+            value={dataAusencia}
+          />
+          <button>Confirmar</button>
+        </form>
       </main>
     </div>
   );
