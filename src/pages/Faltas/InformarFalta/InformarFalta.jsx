82be35c5a22cb9ea5b2c10c3f4dde6066719cd31
“gustavobr1001” <100322388+gustavobraga1001@users.noreply.github.com>
2024-09-03 19:31:58 -0300

    implements toogle button and input days selection

--- src/pages/Faltas/InformarFalta/InformarFalta.jsx
+++ src/pages/Faltas/InformarFalta/InformarFalta.jsx
@@ -7,8 +7,27 @@ export function InfomarFalta() {
   const [ativado, setAtivado] = useState(false);
   const [dataAusencia, setDataAusencia] = useState("");
 
-  const handleClick = () => {
+  const [daysWeek, setDaysWeek] = useState(
+    [
+      { day: "D", enable: false },
+      { day: "S", enable: false },
+      { day: "T", enable: false },
+      { day: "Q", enable: false },
+      { day: "Q", enable: false },
+      { day: "S", enable: false },
+      { day: "S", enable: false },
+    ])
+
+  const handleClick = (i) => {
     setAtivado(!ativado);
+  };
+
+  const handleClickDay = (index) => {
+    setDaysWeek((prevItems) =>
+      prevItems.map((item, i) =>
+        i === index ? { ...item, enable: !item.enable } : item
+      )
+    );
   };
 
   function formatDate(dateString) {
@@ -37,11 +56,12 @@ export function InfomarFalta() {
     setDataAusencia("");
   }
 
+
   return (
     <div>
       <HeaderFixo text={"Informar ausência"} tela="faltas" />
       <main className="main-informar-falta">
-        {/* <div className="box-ausensia">
+        <div className="box-ausensia">
           <span>Ausência recorrente</span>
           <div
             className={`toggle-container ${ativado ? "ativo" : ""}`}
@@ -49,16 +69,33 @@ export function InfomarFalta() {
           >
             <div className="toggle-ball"></div>
           </div>
-        </div> */}
+        </div>
 
         <form onSubmit={handleNewFalta}>
           <label>Data da ausência</label>
-          <input
-            type="date"
-            onChange={(e) => setDataAusencia(e.target.value)}
-            placeholder="00/00/0000"
-            value={dataAusencia}
-          />
+
+          {
+            ativado ? (
+              <div className="options-days">
+                {daysWeek.map((item, i) => (
+                  <div
+                    className={`options-days-item ${item.enable ? "options-days-item-enable" : "options-days-item-disable"} `}
+                    key={i}
+                    onClick={() => handleClickDay(i)}
+                  >
+                    {item.day}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <input
+                type="date"
+                onChange={(e) => setDataAusencia(e.target.value)}
+                placeholder="00/00/0000"
+                value={dataAusencia}
+              />
+            )
+          }
           <button>Confirmar</button>
         </form>
       </main>
